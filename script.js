9ace8fa5d2103a43539fd34e31c4ad9eaf25e253
churanta <churantamondal321@gmail.com>
2023-05-13 21:52:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var container = document.querySelector('.container');
+var loading = document.querySelector('.loading');
+
+// Load the animation
+var animation = lottie.loadAnimation({
+  container: document.getElementById('animation'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets6.lottiefiles.com/packages/lf20_hwm68tlm.json'
+});
+animation.setSpeed(1.9);
+animation.setSubframe(false);
+animation.setQuality('low');
+animation.setSize('20%', '20%');
+
+// Hide the loading message when the animation has loaded
+animation.addEventListener('DOMLoaded', function() {
+  container.removeChild(loading);
+});
